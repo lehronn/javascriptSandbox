d285e3bb8a9b2d28517b8179e81496ead0744dba
Mateusz Stomski <lehronn@gmail.com>
2017-08-23 21:28:06 +0200

    fourth exe, module 8.4, simply var using and math with prompt and alert.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,13 @@
 // scripts.js
 
-var name = prompt('Enter your name');
-alert('Hello, ' + name);
+alert('Twoja przeglądarka ma magiczną moc i potrafi liczyć pole trójkąta, zaraz to sprawdzimy!');
+var a = prompt('Jeśli ten hipotetyczny trójkąt by istniał, to jak długi miałby BOK w [cm]?');
+var h = prompt('Ok, bok miałby... ' + a + ' [cm] hm... wpisz więc teraz WYSOKOŚĆ hipotetycznego, ewentualnego, trójkota Schrödingera.');
+var triangleArea = a*h/2;
+
+alert('Po przejęciu kontroli nad wszystkimi koparkami bitcoinów świata Twoja przeglądarka obliczyła że ten Trójkot Schrödingera ma pole wynoszące, ' + triangleArea + '[cm].');
+
+document.write('Pole Trójkota Schrödingera wynosi: ' + triangleArea + ' [cm].');
 //console.log('Hello, ' + name);
 
 
